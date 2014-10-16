8c25a691a4d7c0d52098d0015d29ed9f6f983375
balika011 <balika011@gmail.com>
2014-10-16 15:05:55 +0200

    Added some virtuals

--- sa_include/CPhysical.h
+++ sa_include/CPhysical.h
@@ -11,6 +11,31 @@ private:
 public:
 	CPhysical();
 	virtual ~CPhysical();
+	
+	/*
+	CPhysical::Add(void)
+	CEntity::Add(CRect const&)
+	CPhysical::Remove(void)
+	CEntity::SetIsStatic(bool)
+	CEntity::SetModelIndex(unsigned int)
+	CEntity::SetModelIndexNoCreate(unsigned int)
+	CEntity::CreateRwObject(void)
+	CEntity::DeleteRwObject(void)
+	CPhysical::GetBoundRect(void)const
+	CPhysical::ProcessControl(void)
+	CPhysical::ProcessCollision(void)
+	CPhysical::ProcessShift(void)
+	CPhysical::TestCollision(bool)
+	CEntity::Teleport(CVector, unsigned char)
+	CEntity::SpecialEntityPreCollisionStuff(CPhysical *, bool, bool &, bool &, bool &, bool &)
+	CEntity::SpecialEntityCalcCollisionSteps(bool &, bool &)
+	CEntity::PreRender(void)
+	CEntity::Render(void)
+	CEntity::SetupLighting(void)
+	CEntity::RemoveLighting(bool)
+	CEntity::FlagToDestroyWhenNextProcessed(void)
+	CPhysical::ProcessEntityCollision(CEntity *, CColPoint *)
+	*/
 };
 
 #endif
